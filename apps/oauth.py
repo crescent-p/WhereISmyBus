214c745d90af4adc380a8fef4da41aa74bfd25de
Crescent <crescentp2003@gmail.com>
2024-11-30 16:20:52 +0530

    removed jose library

--- apps/oauth.py
+++ apps/oauth.py
@@ -1,51 +1,51 @@
-from datetime import datetime, timedelta
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from sqlalchemy.orm import Session
-from .config import settings
-from apps import models, schemas
-from apps.database import get_db
+# from datetime import datetime, timedelta
+# from fastapi import Depends, HTTPException, status
+# from fastapi.security import OAuth2PasswordBearer
+# import jwt
+# from sqlalchemy.orm import Session
+# from .config import settings
+# from apps import models, schemas
+# from apps.database import get_db
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 
 
-SECRET_KEY = settings.secret_key
-ALGORITHM = settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_min
+# SECRET_KEY = settings.secret_key
+# ALGORITHM = settings.algorithm
+# ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_min
 
 
-def create_access_token(data: dict):
-    to_encode = data.copy()
+# def create_access_token(data: dict):
+#     to_encode = data.copy()
 
-    expire = datetime.utcnow()+ timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    print(datetime.now())
-    to_encode.update({"exp": expire})
+#     expire = datetime.utcnow()+ timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     print(datetime.now())
+#     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-    return encoded_jwt
+#     return encoded_jwt
  
 
-def verify_access_token(token: str, credential_exception):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+# def verify_access_token(token: str, credential_exception):
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-        id: str = payload.get("user_id")
+#         id: str = payload.get("user_id")
 
-        if not id:
-            raise credential_exception
-        token_data = schemas.TokenData(id=id)
-    except JWTError:
-        raise credential_exception
-    return token_data
+#         if not id:
+#             raise credential_exception
+#         token_data = schemas.TokenData(id=id)
+#     except jwt.:
+#         raise credential_exception
+#     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Couldn't verify token")
+# def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+#     credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Couldn't verify token")
 
-    token = verify_access_token(token, credential_exception=credential_exception)
+#     token = verify_access_token(token, credential_exception=credential_exception)
 
-    user = db.query(models.Admin).where(models.Admin.id == token.id).first()
+#     user = db.query(models.Admin).where(models.Admin.id == token.id).first()
 
-    return user
+#     return user
